e5cdef1016392b13481e2e2b4549b9a422e01745
coachpete2008 <coachpete2008@gmail.com>
2020-05-15 21:55:25 +0000

    more files

---
+++ bashrc
@@ -0,0 +1,8 @@
+alias mop="clear"
+echo "Hello John"
+source .bashrc
+
+#move to the bashrc to original
+cp bashrc_backup .bashrc
+source .bashrc
+
---
+++ conditions.py
@@ -0,0 +1,15 @@
+#This program introduces variables, operators, conditions
+name="Raj"
+age=22
+
+brothername="Vijay"
+brotherage=26
+
+agedifference=age-brotherage
+difference=abs(agedifference)
+
+print("My Name is:",name)
+print("My Age is:",age)
+print("My Brothers Name is:", brothername)
+print("My Brothers Age is:", brotherage)
+print("We were born",difference,"years apart")
